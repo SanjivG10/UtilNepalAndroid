2408f7792d760b7a0be05ed2c9a17e26135d67cf
SanjivG <sanjivgautamofficial@gmail.com>
2018-11-16 09:49:46 +0545

    Almost Features are finished

--- app/src/main/java/com/utilnepal/MobileHelp/adapters/EmergencyNumberAdapter.java
+++ app/src/main/java/com/utilnepal/MobileHelp/adapters/EmergencyNumberAdapter.java
@@ -1,11 +1,15 @@
 package com.utilnepal.MobileHelp.adapters;
 
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.utilnepal.MobileHelp.Files.EmergencyNumberFeatures;
@@ -37,9 +41,32 @@ public class EmergencyNumberAdapter extends RecyclerView.Adapter<EmergencyNumber
     }
 
     @Override
-    public void onBindViewHolder(@NonNull MyViewHolder myViewHolder, int i) {
+    public void onBindViewHolder(@NonNull MyViewHolder myViewHolder, final int i) {
         myViewHolder.emergencyPlaceName.setText(emergencyNumbers.get(i).getEmergencyPlaceName());
         myViewHolder.emergencyPlaceNumber.setText(emergencyNumbers.get(i).getEmergencyPlaceNumber());
+
+        myViewHolder.callingButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String number  =    emergencyNumbers.get(i).getEmergencyPlaceNumber();
+                if(number.length() >3)
+                {
+                    if(!number.startsWith("01"))
+                    {
+                        number = "01"+number;
+                    }
+                    Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + Uri.encode(number)));
+                    c.startActivity(intent);
+                    Log.e("Number", number);
+                }
+                else if(number.length()==3)
+                {
+                    Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + Uri.encode(number)));
+                    c.startActivity(intent);
+                }
+
+            }
+        });
 
     }
 
@@ -52,12 +79,14 @@ public class EmergencyNumberAdapter extends RecyclerView.Adapter<EmergencyNumber
     {
         private TextView emergencyPlaceName;
         private TextView emergencyPlaceNumber;
+        private ImageView callingButton;
 
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
             emergencyPlaceName = itemView.findViewById(R.id.nameOfEmergencyPlace);
             emergencyPlaceNumber = itemView.findViewById(R.id.numberOfEmergencyPlace);
+            callingButton = itemView.findViewById(R.id.callingButton);
         }
     }
 }
